9c279611969baa69145a60c225ece3e1beebb105
kri-sh27 <hogalekrishna2000@gmail.com>
2023-10-12 11:45:32 +0530

    updated

--- BookBackend/dist/app.module.js
+++ BookBackend/dist/app.module.js
@@ -21,7 +21,7 @@ AppModule = __decorate([
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'mysql',
                 host: 'localhost',
-                port: 3306,
+                port: 3307,
                 username: 'root',
                 password: 'root',
                 database: 'bookdb',
